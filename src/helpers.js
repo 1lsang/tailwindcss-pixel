619d362357fbf0f37fbc2d18192a532929a52a0d
1lsang <ilsangpark.98@gmail.com>
2023-08-25 08:30:33 +0900

    [FEAT] add pxr function for making pixel rem objects

--- src/helpers.js
+++ src/helpers.js
@@ -14,8 +14,17 @@ function px(start = 0, stop = 1024, step = 1, suffix = 'px') {
     }
 }
 
+function pxr(start = 0, stop = 1024, step = 1, suffix = 'pxr') {
+    return {
+        ...range(start, stop, step).reduce((acc, px) => {
+            acc[`${px}${suffix ? suffix : ''}`] = pxToRem(px);
+            return acc;
+        }, {}),
+    };
+}
+
 function pxToRem(px, base = 16) {
     return `${px / base}rem`;
 }
 
-export { range, px }
+export { range, px, pxr }
