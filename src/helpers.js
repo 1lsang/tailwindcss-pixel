fea4f8f21820c136c3803f47ba39e86e715852ab
1lsang <ilsangpark.98@gmail.com>
2023-08-25 08:28:08 +0900

    [FEAT] add px function for making pixel objects

--- src/helpers.js
+++ src/helpers.js
@@ -5,4 +5,13 @@ function range(start, stop, step = 1) {
     );
 }
 
-export { range }
+function px(start = 0, stop = 1024, step = 1, suffix = 'px') {
+    return {
+        ...range(start, stop, step).reduce((acc, px) => {
+            acc[`${px}${suffix ? suffix : ''}`] = `${px}px`
+            return acc;
+        }, {}),
+    }
+}
+
+export { range, px }
